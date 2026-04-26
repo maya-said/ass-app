ae73c743eeaac60604c7efff83d49409c2bd383b
maya <mayasaid225@gmail.com>
2026-04-27 02:01:21 +0300

    update slider component

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -2,7 +2,7 @@ function Slider() {
   return (
     <div className="bg-light p-5 text-center">
       <h1>Welcome to My Website</h1>
-      <p>This is my React project 🚀</p>
+      <p>This is My React project 🚀</p>
     </div>
   );
 }
