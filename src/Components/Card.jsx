db766a28895b9f8d3474031ea75084809582bf6f
maya <mayasaid225@gmail.com>
2026-04-27 01:58:11 +0300

    update card component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ function Card(props) {
     <div className="card shadow p-3 mb-4">
       <div className="card-body text-center">
         <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">This is a simple card</p>
+        <p className="card-text">This is my first cared</p>
         <button className="btn btn-primary">Click</button>
       </div>
     </div>
